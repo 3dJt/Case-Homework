3fe9d4419c91cb89ab9ef54c226676701b80e669
Anton <antonkalash037@mail.ru>
2024-05-15 18:13:59 +0300

    Ready

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-year = int(input("Введите год: "))
+def is_year_leap(year):
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        print(True)
+    else:
+        print(False)
 
-if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    print(True)
-else:
-    print(False)
+is_year_leap(2024)
